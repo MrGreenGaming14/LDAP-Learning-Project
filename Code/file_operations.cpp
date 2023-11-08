c3e88e26b94915dc15d9290b1fa756254ef3146b
Ethan Purser <epurse2@lsu.edu>
2023-11-07 22:57:52 -0600

    cleans up code by splitting into more functions

--- Code/file_operations.cpp
+++ Code/file_operations.cpp
@@ -9,7 +9,9 @@
 
 //voids will return ints for "error codes" 0 for passing and -1 for failing
 int create_file(std::string const& filename);
-int parse_input(std::string const& command, std::string const& param);
+int parse_input(std::string& command, std::string& param);
+int execute_command(std::string& command, std::string& param, 
+    std::unordered_map<std::string, std::function<int(std::string)>>& commands);
 
 int main(int argc, char** argv)
 {
@@ -20,8 +22,9 @@ int main(int argc, char** argv)
     std::unordered_map<std::string, std::function<int(std::string)>> commands = {
         {"create_file", &create_file},
         {"help", [param](std::string/*useless param for syntax reasons*/){
-            std::cout << "Usage: [command] "
-            << "[parameter, if applicable] " << std::endl;
+            std::cout << "Usage:  -[command] "
+            << "[parameter, if applicable] " << std::endl
+            << "\t-Type \"quit\" to quit." << std::endl;
             return 0;
         }}
         //{"stop", &stop},
@@ -31,38 +34,17 @@ int main(int argc, char** argv)
         << "(name subject to change.)\" " 
         << "Type \"help\" for a list of commands." << std::endl;
     
+        //program loop
     while(run)
     {
-        command.erase(); param.erase(); //clear commands
-            //gets whole input
-        std::getline(std::cin, command);
-            //gets the command and param
-        std::istringstream iss(command);
-            //gets the command like cin
-        iss >> command;
-            //puts param in param
-        param = iss.str().substr(command.size());
-        if (!param.empty() && param[0] == ' ') {
-            param.erase(0, 1); // Remove the leading space
-        }
+        parse_input(command, param);
         if(command == "quit")
         {
             run = false;
             break;
         }
         //executes command
-        auto it = commands.find(command);
-        if (it != commands.end()) {
-            //call the function associated with the command
-            int result = it->second(param);
-            if (result == 0) {
-                std::cout << "Command executed successfully." << std::endl;
-            } else {
-                std::cout << "Command failed." << std::endl;
-            }
-        } else {
-            std::cout << "Unknown command or input format." << std::endl;
-        }
+        execute_command(command, param, commands);
     }
     
     return 0;
@@ -85,6 +67,42 @@ int create_file(std::string const& filename)
 
     // Close the file
     outFile.close();
+
+    return 0;
+}
+
+int parse_input(std::string& command, std::string& param)
+{
+        //gets whole input
+    std::getline(std::cin, command);
+        //gets the command and param
+    std::istringstream iss(command);
+        //gets the command like cin
+    iss >> command;
+        //puts param in param
+    param = iss.str().substr(command.size());
+    if (!param.empty() && param[0] == ' ') {
+        param.erase(0, 1); // Remove the leading space
+    }
+
+    return 0;
+}
+
+int execute_command(std::string& command, std::string& param, 
+    std::unordered_map<std::string, std::function<int(std::string)>>& commands)
+{
+    auto it = commands.find(command);
+    if (it != commands.end()) {
+        //call the function associated with the command
+        int result = it->second(param);
+        if (result == 0) {
+            std::cout << "Command executed successfully." << std::endl;
+        } else {
+            std::cout << "Command failed." << std::endl;
+        }
+    } else {
+        std::cout << "Unknown command or input format." << std::endl;
+    }
 
     return 0;
 }
