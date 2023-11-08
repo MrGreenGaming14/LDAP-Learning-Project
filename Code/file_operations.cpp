009e4d6ca5382d9aa5858e26932e60911cbf61eb
Ethan Purser <epurse2@lsu.edu>
2023-11-08 15:20:54 -0600

    added file deletion

--- Code/file_operations.cpp
+++ Code/file_operations.cpp
@@ -12,6 +12,7 @@ int create_file(std::string const& filename);
 int parse_input(std::string& command, std::string& param);
 int execute_command(std::string& command, std::string& param, 
     std::unordered_map<std::string, std::function<int(std::string)>>& commands);
+int delete_file(std::string const& filename);
 
 int main(int argc, char** argv)
 {
@@ -21,11 +22,14 @@ int main(int argc, char** argv)
         //maps commands to functions
     std::unordered_map<std::string, std::function<int(std::string)>> commands = {
         {"create_file", &create_file},
+        {"delete_file", &delete_file},
         {"help", [param](std::string/*useless param for syntax reasons*/){
             std::cout << "Usage:  -[command] "
             << "[parameter, if applicable] " << std::endl
             << "\t-Type \"quit\" to quit." << std::endl
-            << "\t-create_file [filename.extension]" << std::endl;
+            << "\t-create_file [filename.extension]" << std::endl
+            << "\t-delete_file [filename.extension]" << std::endl;
+            
             return 0;
         }}
         //{"stop", &stop},
@@ -105,5 +109,26 @@ int execute_command(std::string& command, std::string& param,
         std::cout << "Unknown command or input format." << std::endl;
     }
 
+    return 0;
+}
+
+    //-1 if file does not exist, -2 for other unknown error
+int delete_file(std::string const& filename)
+{
+    try {
+        // Check if the file exists before attempting to delete it
+        if (std::filesystem::exists("../Files/" + filename)) {
+            // Delete the file
+            std::filesystem::remove("../Files/" + filename);
+            //std::cout << "File deleted successfully." << std::endl;
+        } else {
+            //std::cout << "File does not exist." << std::endl;
+            return -1;
+        }
+    } catch (const std::filesystem::filesystem_error& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return -2;
+    }
+    
     return 0;
 }
